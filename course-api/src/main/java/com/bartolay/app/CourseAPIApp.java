cbd7413a6579500c1d48c6fb76eee463e8485ae8
rbartolay <rbartolay@192.168.254.6>
2018-02-03 07:55:34 +0800

    moved packages for component scanning

---
+++ course-api/src/main/java/com/bartolay/app/CourseAPIApp.java
@@ -0,0 +1,11 @@
+package com.bartolay.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAPIApp {
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAPIApp.class, args);
+	}
+}
